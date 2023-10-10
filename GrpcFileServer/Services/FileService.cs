3e4940a1fa0e5c2ca983f57bf052cad6062acc3c
toksozhalil <toksozhalil@gmail.com>
2023-10-10 14:20:06 +0300

    feat: added new try-catch exception handlers

--- GrpcFileServer/Services/FileService.cs
+++ GrpcFileServer/Services/FileService.cs
@@ -1,7 +1,6 @@
 ﻿using Google.Protobuf;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
-using System.IO;
 using static GrpcFileServer.FileService;
 
 namespace GrpcFileServer.Services
@@ -30,31 +29,20 @@ namespace GrpcFileServer.Services
 
                 while (await requestStream.MoveNext())
                 {
-                    //Stream ilk başladığında(ilk adımda) yapılması gereken öncelikli işlevler gerçekleştiriliyor.
+                    //Stream ilk başladığında(ilk adımda) yapılması gereken öncelikli işlevler
                     if (count++ == 0)
                     {
-                        //Stream'de gelen Info nesnesinin FileName özelliğiyle hedef dosyanın adı belirleniyor.
                         string filePath = $"{path}/{requestStream.Current.Info.FileName}{requestStream.Current.Info.FileExtension}";
                         fileStream = new FileStream(filePath, FileMode.CreateNew);
 
-                        //Gelecek dosya boyutu kadar alan tahsis ediliyor. Bu işlem zorunlu değildir
-                        //lakin süreçte farklı bir program tarafından diskin doldurulup, işimize engel olmasının önüne geçiyoruz.
+                        //Gelecek dosya boyutu kadar alan tahsis ediliyor.
                         fileStream.SetLength(requestStream.Current.FileSize);
                     }
 
-                    //Buffer, akışta gelen her bir parçanın ta kendisidir. Chunk olarak isimlendirilir.
                     var buffer = requestStream.Current.Buffer.ToByteArray();
 
-                    //Akışta gelen chunk'ları hedef FileStream nesnesine yazdırıyoruz.
-                    //Burada, ikinci parametrede ki '0' değeri ile buffer'dan kaçıncı byte'dan itibaren okunacağı ve
-                    //yazdırılacağı bildirilmektedir.
                     await fileStream.WriteAsync(buffer, 0, requestStream.Current.ReadedByte);
 
-                    //Akışın yüzdelik olarak ne kadarının aktarıldığı hesaplanıyor.
-                    //Formülasyon olarak;
-                    //Okunan parça sayısı(ReadedByte), chunkSize değişkeninde toplanıyor ve 100 ile çarpılıp
-                    //sonuç toplam boyuta bölünüyor. Nihai sonuç ise yakın olan tam sayıya yuvarlanıyor ve
-                    //yüzdelik olarak ne kadarlık aktarım gerçekleştirildiği hesaplanmış oluyor.
                     chunkSize += requestStream.Current.ReadedByte;
                     decimal uploadedRate = Math.Round((chunkSize * 100) / requestStream.Current.FileSize);
                     Console.WriteLine($"{uploadedRate}%");
@@ -64,12 +52,14 @@ namespace GrpcFileServer.Services
             }
             catch (Exception ex)
             {
-                //Client'ta stream 'CompleteAsync' edildiği vakit burada olası hata meydana gelebilmektedir.
-                //Dolayısıyla tüm bu süreci try catch ile kontrol ediyoruz.
                 Console.WriteLine("Exception: " + ex.ToString());
             }
-            await fileStream.DisposeAsync();
-            fileStream.Close();
+            finally { 
+                if (fileStream != null) { 
+                    await fileStream.DisposeAsync();
+                    fileStream.Close();
+                } 
+            }
             return new Empty();
         }
 
@@ -78,25 +68,33 @@ namespace GrpcFileServer.Services
             string path = Path.Combine(_webHostEnvironment.WebRootPath, "files");
 
             string filePath = $"{path}/{request.FileName}{request.FileExtension}";
-            using FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
-
-            byte[] buffer = new byte[2048];
-
-            BytesContent content = new BytesContent
+            try
             {
-                FileSize = fileStream.Length,
-                Info = new FileInfo { FileName = request.FileName + "_gRPC_FileService", FileExtension = request.FileExtension },
-                ReadedByte = 0
-            };
+                bool isFileExist = File.Exists(filePath);
+                if (!isFileExist) throw new FileNotFoundException(filePath);
 
-            while ((content.ReadedByte = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                using FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+                byte[] buffer = new byte[2048];
+
+                BytesContent content = new BytesContent
+                {
+                    FileSize = fileStream.Length,
+                    Info = new FileInfo { FileName = request.FileName + "_gRPC_FileService", FileExtension = request.FileExtension },
+                    ReadedByte = 0
+                };
+
+                while ((content.ReadedByte = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    //Okunan buffer'ın stream edilebilmesi için 'message.proto' dosyasındaki 'bytes' türüne dönüştürülüyor.
+                    content.Buffer = ByteString.CopyFrom(buffer);
+                    await responseStream.WriteAsync(content);
+                }
+                fileStream.Close();
+            }catch (Exception ex)
             {
-                //Okunan buffer'ın stream edilebilmesi için 'message.proto' dosyasındaki 'bytes' türüne dönüştürülüyor.
-                content.Buffer = ByteString.CopyFrom(buffer);
-                await responseStream.WriteAsync(content);
+                Console.WriteLine(ex.ToString());
             }
-
-            fileStream.Close();
         }
 
         public override Task<FilePath> GetFilePath(FileInfo request, ServerCallContext context)
